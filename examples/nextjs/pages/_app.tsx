f009924df3006d8cff5509fa382dc07bd817f157
Ashtechhacks <104894013+Ashtechhacks@users.noreply.github.com>
2022-11-25 06:47:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ examples/nextjs/pages/_app.tsx
@@ -0,0 +1,26 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import { WagmiConfig, createClient, chain } from 'wagmi';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+
+const client = createClient(
+  getDefaultClient({
+    appName: 'ConnectKit Next.js demo',
+    //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    chains: [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  })
+);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
